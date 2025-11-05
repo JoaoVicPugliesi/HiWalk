ebe75ca97c9cc02c9b81b91bbcadd84d4b62c2a3
JoaoVicPugliesi <mrlanguages62@gmail.com>
2025-11-05 07:56:01 -0300

    Add create button and initialize calendar section

--- js/parallax/personal-1-change-container.js
+++ js/parallax/personal-1-change-container.js
@@ -4,11 +4,14 @@ const changeContainer = () => {
     const containerIconLink = document.querySelectorAll('.container-icon-link');
     const playBtn = document.querySelector('.play-btn');
     const muteBtn = document.querySelector('.mute-btn');
+    const createBtn = document.querySelector('.create-btn');
     const destinations = document.querySelector('#destinations');
+    const calendar = document.querySelector('#calendar');
 
-    if (!container || !destinations) return;
+    if (!container || !destinations || !calendar) return;
 
     const destinationTop = destinations.offsetTop;
+    const calendarTop = calendar.offsetTop;
     const scrollPosition = window.scrollY + window.innerHeight / 2;
 
     
@@ -20,13 +23,17 @@ const changeContainer = () => {
         container.classList.add('change'); 
         playBtn.classList.add('change'); 
         muteBtn.classList.add('change'); 
-    } else {
+        createBtn.classList.add('change'); 
+    } 
+    
+    if(scrollPosition < destinationTop || scrollPosition >= calendarTop) {
         containerIconLink.forEach(ancher => {
             ancher.classList.remove('change'); 
         });
         container.classList.remove('change'); 
         playBtn.classList.remove('change'); 
         muteBtn.classList.remove('change'); 
+        createBtn.classList.remove('change'); 
     }
 
     console.log("changing");
